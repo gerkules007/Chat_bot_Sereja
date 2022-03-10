1b5f3acc2d620921913f92ad4054d68778585917
OvcS <gerkules007@yahoo.com>
2022-03-10 22:18:44 +0300

    Update functional for bot's answer

--- comands.py
+++ comands.py
@@ -1,29 +1,45 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import random 
+import json
 
 class AI_BOT:
-    answer = {
-    "привет": "Привет, человек",
-    "зачем ты здесь": "чтобы радовать тебя",
-    "ок": ""
+    key_talk = {
+        ("привет", "здарова", "здарово", "прив"): "hello",
+        ("зачем ты здесь"): "why",
+        ("хорошо", "ок"): "OK"
     }
-    talk = [
-        "Не понимаю", "Повтори еще раз", "Возможно тебе стоит подучить русский язык"
+    answer = {
+        "hello": ["привет, человек", "привет", "здаров"],
+        "why": ["чтобы радовать тебя", "а ты зачем здесь?"],
+        "OK": ["к", ""]
+    }
+    not_answer = [
+        "Не понимаю", 
+        "Повтори еще раз", 
+        "Возможно тебе стоит подучить русский язык", 
+        "Спроси что-то попроще"
     ]
 
+def rand_item(rand_list):
+    return random.randint(0, len(rand_list)-1)
 
 def answer(update: Update, context: CallbackContext):
     user_message: str = update.message.text.lower()
-    answer_bot = AI_BOT()
+    ab = AI_BOT()
     user_key = user_message
 
     try:
-        if user_key in answer_bot.answer.keys():
-            update.message.reply_text(answer_bot.answer[user_key])
+        for key in ab.key_talk.items():
+            
+            if user_key in key[0]:
+                update.message.reply_text( \
+                    ab.answer[key[1]]
+                        [rand_item(ab.answer)])
+                return
         else:
             update.message.reply_text( \
-                answer_bot.talk[ \
-                    random.randint(0, len(answer_bot.talk)-1)])
+                ab.not_answer[ \
+                    rand_item(ab.not_answer)])
     except Exception:
         return
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, ConversationHandler
-from comands import *
+from comands import answer
 
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
@@ -9,6 +9,8 @@ with open("token.txt", "r") as token:
     updater = Updater(token.read())
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+# добавить команду help, которая появляется сразу после старта
+# добавить команду для добавления ошибок и разговора
 updater.dispatcher.add_handler(MessageHandler(Filters.text, answer))
 
 updater.start_polling()
